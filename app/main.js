bb0e45c7c07dc1d77ce32fc638e161a1a2dfbf6f
Sakshi Chopkar <sakshichopkar.liv@gmail.com>
2025-07-13 20:06:45 +0100

    Echo command

--- app/main.js
+++ app/main.js
@@ -7,8 +7,34 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle connection
   connection.on('data', (data) => {
-    connection.write("+PONG\r\n");
+    const cmdArr = parseRESP(data);
+
+        if (cmdArr && cmdArr[0] && cmdArr[0].toLowerCase() === "echo") {
+            const message = cmdArr[1] || "";
+            const resp = `$${message.length}\r\n${message}\r\n`;
+            connection.write(resp);
+        } else {
+            connection.write("+PONG\r\n");
+        }
   })
 });
 
 server.listen(6379, "127.0.0.1");
+
+// RESP parser function
+function parseRESP(buffer) {
+    const str = buffer.toString();
+
+    if (str[0] !== '*') {
+        return null;
+    }
+
+    const parts = str.split('\r\n').filter(Boolean);
+
+    let arr = [];
+    for (let i = 2; i < parts.length; i += 2) {
+        arr.push(parts[i]);
+    }
+
+    return arr;
+}
